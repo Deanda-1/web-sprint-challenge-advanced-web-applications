7b5f1d51242276ab8f74e9182d90f16cb88d1727
DJ1 <jimmynoble604@gmail.com>
2023-08-28 17:50:59 -0400

    index.js

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,13 @@
 // ✨ implement axiosWithAuth
+import Axios from "axios";
+
+
+
+expect function axiosWithAuth() {
+    const token = localStorage.getItem('token')
+    return axios.create({
+        header: {
+            Authorization: token, 
+        },
+    })
+}
